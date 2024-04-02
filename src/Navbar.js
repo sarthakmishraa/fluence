b4120249790d09c42eaac70829015c34ad6418d7
Sarthak Mishra <msarthak785@gmail.com>
2024-04-02 18:17:33 -0400

    Responsive style properties added

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,19 +6,17 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="Navbar">
-      <div className="NavbarLeft">
-        <div className='FooterName'>
-          <h1 className='FooterNameP1'>flu</h1>
-          <h1 className='FooterNameP2'>ence</h1>
-        </div>
-        <nav className="NavbarLeftItems">
-          <Link to="/">Home</Link>
-          <Link to="/visualisations">Visualisations</Link>
-          <Link to="/methodology">Methodology</Link>
-          <Link to="/about">About</Link>
-          <Link to="https://adiimated.github.io/portfolio/#contact" target="_blank">Contact</Link>
-        </nav>
+      <div className='FooterName'>
+        <h1 className='FooterNameP1'>flu</h1>
+        <h1 className='FooterNameP2'>ence</h1>
       </div>
+      <nav className="NavbarLeftItems">
+        <Link to="/">Home</Link>
+        <Link to="/visualisations">Visualisations</Link>
+        <Link to="/methodology">Methodology</Link>
+        <Link to="/about">About</Link>
+        <Link to="https://adiimated.github.io/portfolio/#contact" target="_blank">Contact</Link>
+      </nav>
     </div>
   );
 }
